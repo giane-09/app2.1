0e9489fd3c35115b4c458787ed5a5b83bcd0f8e9
giane-09 <gianelaamv@gmail.com>
2022-10-18 19:39:00 +0100

    final app2

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,83 @@ class MyApp extends StatelessWidget {
           children: [
             CircleAvatar(radius: 60.0,
             backgroundColor: Colors.white,
-            backgroundImage: NetworkImage("https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg"),
+            backgroundImage: AssetImage("assets/images/person2.jpg"),
             ),
+            Text(
+              "Fiorela de Fatima Guadalupe", style: TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: "dancing"
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(color: Colors.white38, 
+                fontSize: 18.0,
+                fontWeight: FontWeight.w300,
+                letterSpacing: 2.0,
+                ),
+              ),
+              Divider(
+                thickness: 0.70,
+                color: Colors.white38,
+                indent: 100.0,
+                endIndent: 100.0,
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                elevation: 10.0,
+                child: ListTile(
+                  title: Text("+51 924562574"),
+                  subtitle: Text("Telefono"),
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.indigo,),
+                  trailing: Icon(Icons.check_circle_outline,
+                  color: Colors.indigo,
+                  ),
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                elevation: 10.0,
+                child: ListTile(
+                  title: Text("gianelaamv@gmail.com"),
+                  subtitle: Text("correo electronico"),
+                  leading: Icon(
+                    Icons.mail, 
+                    color: Colors.indigo,),
+                  trailing: Icon(Icons.check_circle_outline,
+                  color: Colors.indigo,
+                  ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/facebook.png",
+                height: 60.0,
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Image.asset("assets/images/instagram.png",
+                height: 60.0,
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Image.asset("assets/images/twitter.png",
+                height: 60.0,
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+              ],
+            )
           ],
         ),
       ),
